85ea13baab7a7cfbf3e88995d8067f74f9a7eb0a
Diman12345 <wardimanperdian@gmail.com>
2018-12-04 19:32:01 +0700

    menu
    menu

--- Assets/manageUI.cs
+++ Assets/manageUI.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class manageUI : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+	}
+
+    public void Play() {
+        Application.LoadLevel ("Level1");
 	}
 
     // fungsi untuk pause
